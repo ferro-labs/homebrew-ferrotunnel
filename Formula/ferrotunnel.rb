e639a398eac5898200ecce8fbb166e50b12fa4fe
Mitul Shah <shahmitul005@gmail.com>
2026-04-09 12:17:01 +0530

    Update formula for v1.0.7

--- Formula/ferrotunnel.rb
+++ Formula/ferrotunnel.rb
@@ -1,8 +1,8 @@
 class Ferrotunnel < Formula
   desc "Embeddable, extensible, and observable reverse tunnel for Rust developers"
   homepage "https://github.com/ferro-labs/ferrotunnel"
-  url "https://github.com/ferro-labs/ferrotunnel/archive/refs/tags/v1.0.6.tar.gz"
-  sha256 "85d593cc586a6a2230f9bf0a5b17298e587434fbe12c4a2210bbac6095f091ca"
+  url "https://github.com/ferro-labs/ferrotunnel/archive/refs/tags/v1.0.7.tar.gz"
+  sha256 "c31f1eca979e69635724b5f574284f11be9cbf4ca421a3977737d087e2f70e81"
   license "MIT OR Apache-2.0"
   head "https://github.com/ferro-labs/ferrotunnel.git", branch: "main"
 
